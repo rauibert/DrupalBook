dfbbfefa87143e36a34609f90bc5904fcf25272f
Rauibert <rauibertnf@gmail.com>
2023-03-10 17:46:11 +0100

    Added Paragrraphs module

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -426,9 +426,9 @@ class ComposerStaticInitdba652fc9b1d80a4571c71375e804094
         ),
         'Doctrine\\Common\\' => 
         array (
-            0 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
+            0 => __DIR__ . '/..' . '/doctrine/common/src',
             1 => __DIR__ . '/..' . '/doctrine/event-manager/src',
-            2 => __DIR__ . '/..' . '/doctrine/common/src',
+            2 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
         ),
         'Dflydev\\DotAccessData\\' => 
         array (
